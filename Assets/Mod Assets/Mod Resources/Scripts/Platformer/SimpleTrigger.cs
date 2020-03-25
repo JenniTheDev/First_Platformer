93848b1d27c9e7944eb6ed7ed3589560dbc882cf
Vast <Vastlee@gmail.com>
2020-03-25 16:16:04 -0700

    Added Confetti Explosion on Enemy Contact
    Added Trigger to Player Prefab
    Added Confetti Particles to Enemy Prefab
    Changed SimpleTrigger to find player tag & assign RigidBody on Start.

--- Assets/Mod Assets/Mod Resources/Scripts/Platformer/SimpleTrigger.cs
+++ Assets/Mod Assets/Mod Resources/Scripts/Platformer/SimpleTrigger.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class SimpleTrigger : MonoBehaviour
-{
+public class SimpleTrigger : MonoBehaviour {
+    [SerializeField] private Rigidbody2D triggerBody;
+    [SerializeField] private UnityEvent onTriggerEnter;
 
-    public Rigidbody2D triggerBody; 
-    public UnityEvent onTriggerEnter;
+    #region MonoBehaviour
+    private void Start() {
+        AssignPlayerRB();
+    }
 
-
-    void OnTriggerEnter2D(Collider2D other){
-        //do not trigger if there's no trigger target object
-        if (triggerBody == null) return;
-
-        //only trigger if the triggerBody matches
+    void OnTriggerEnter2D(Collider2D other) {
         var hitRb = other.attachedRigidbody;
-        if (hitRb == triggerBody){
+        if(hitRb == triggerBody) {
             onTriggerEnter.Invoke();
         }
     }
+    #endregion
 
+    #region Methods
+    private void AssignPlayerRB() {
+        if(triggerBody == null) {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            if(players.Length == 0) {
+                Debug.LogError("No Objects with Tag Player");
+                Debug.Break();
+            } else if(players.Length > 1) {
+                Debug.LogError("Multiple Objects with Tag Player");
+                Debug.Break();
+            }
+            triggerBody = players[0].GetComponent<Rigidbody2D>();
+        }
+    }
+    #endregion
 }
